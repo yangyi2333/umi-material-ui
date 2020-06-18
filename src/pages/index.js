2c2e00a4e3e5b4beb56c68719cecb35f35dfbf52
“ <535261679@qq.com>
2020-06-19 03:19:48 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import styles from './index.css';
+import {Button,Container } from '@material-ui/core';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      {/*<div className={styles.welcome} />*/}
+      {/*<ul className={styles.list}>*/}
+      {/*  <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>*/}
+      {/*  <li>*/}
+      {/*    <a href="https://umijs.org/guide/getting-started.html">*/}
+      {/*      Getting Started*/}
+      {/*    </a>*/}
+      {/*  </li>*/}
+      {/*</ul>*/}
+      <Container maxWidth="md">
+        <Button>123456</Button>
+      </Container>
+    </div>
+  );
+}
